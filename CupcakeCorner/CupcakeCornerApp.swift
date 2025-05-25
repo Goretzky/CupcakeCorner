0773371d6b9662a255ce74c6ab60e405c318e500
Kern Goretzky <kern.goretzky@gmail.com>
2025-05-25 13:37:53 -0400

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kern Goretzky on 2025-05-25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
